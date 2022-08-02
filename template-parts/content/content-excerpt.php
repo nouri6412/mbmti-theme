f21bbc85b30e7a1868909fb0bc988756e713744b
Jalil Nouri <nouri.6412@gmail.com>
2022-08-02 16:30:22 +0430

    fix bug

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -21,6 +21,6 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
+		<?php //twenty_twenty_one_entry_meta_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-${ID} -->
